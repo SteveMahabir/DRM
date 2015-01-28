9d85ae1a46661a9182993d86a0de2ac98b9fb681
Steve Mahabir <sw.mahabir@gmail.com>
2015-01-27 23:43:43 -0500

    Initial Template Solution
    Initial solution with three projects:
    1) Presentation Layer
    2) Business Layer
    3) Transferable Objects (ViewModels)

---
+++ DRM/DRM/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace DRM
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ DRM/DRM/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DRM.MvcApplication" Language="C#" %>
