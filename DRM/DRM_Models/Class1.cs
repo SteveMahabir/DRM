653da9b7eff9bfe44c75c07df4aa422211832fa5
Steve Mahabir <sw.mahabir@gmail.com>
2015-01-29 17:17:04 -0500

    Added Database
    Created a local database and added it to the solution.
    Do alter this database you can import it in SQL Management Studio (SSMS)
    or use Visual Studio's model editor to edit the model.
    
    We need to test this on all of our computers!

--- DRM/DRM_Models/Class1.cs
+++ DRM/DRM_Models/Class1.cs
@@ -8,5 +8,10 @@ namespace DRM_Models
 {
     public class Class1
     {
+        public void main()
+        { 
+            DatabaseEntities data = new DatabaseEntities();
+            data.SaveChanges();
+        }
     }
 }
